3ebdaa0b90c0b60ae619a36993870e7e70624051
wardtyl <wardtyl@OA-S318-24>
2026-03-23 18:06:57 -0400

    Work in progress

---
+++ Assets/Scripts/StrategyBuilder.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class StrategyBuilder : MonoBehaviour
+{
+    public GameObject house;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnClick()
+    {
+
+    }
+}
