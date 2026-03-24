f3f88f14c80611c9f4344dd40fa6d3dd10d4051c
wardtyl <wardtyl@OA-S318-24>
2026-03-23 20:21:06 -0400

    first step of the strategy builder complete

--- Assets/Scripts/StrategyBuilder.cs
+++ Assets/Scripts/StrategyBuilder.cs
@@ -1,8 +1,16 @@
+using UnityEditor.Build;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class StrategyBuilder : MonoBehaviour
 {
-    public GameObject house;
+    private float Progress = 0f;
+    private float destroyProgress = 0f;
+
+    public float Duration;
+    public float destroyDuration;
+
+    public GameObject houseBuilder;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -12,11 +20,18 @@ public class StrategyBuilder : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+     
     }
 
-    public void OnClick()
+    public void OnClick(InputAction.CallbackContext context)
     {
+        Progress += Time.deltaTime;
+       
+            GameObject spawnedObject = Instantiate(houseBuilder, transform.position, Quaternion.identity);
 
+            Destroy(spawnedObject, destroyDuration);
+
+            Progress = 0f;
+        
     }
 }
